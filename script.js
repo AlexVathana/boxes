6c5277f466802ec647b4761d5790b3256c3a4d93
AlexVathana <119772863+AlexVathana@users.noreply.github.com>
2024-06-12 09:52:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
+const naButton = document.getElementById("naButton");
+const euButton = document.getElementById("euButton");
+const saButton = document.getElementById("saButton");
+const asiaButton = document.getElementById("asiaButton");
+const showAllButton = document.getElementById("allButton");
+const boxes = document.querySelectorAll(".box");
 
-    const naButton = document.getElementById("naButton");
-    const euButton = document.getElementById("euButton");
-    const saButton = document.getElementById("saButton");
-    const asiaButton = document.getElementById("asiaButton");
-    const showAllButton = document.getElementById("allButton");
-    const boxes = document.querySelectorAll(".box");
+
+const originalOrder = Array.from(boxes);
+
+const filterBoxes = (continent) => {
+    const container = document.querySelector(".container");
 
 
-    const filterBoxes = (continent) => {
+    if (continent === "all") {
+        originalOrder.forEach((box) => {
+            box.classList.remove("hidden");
+            container.appendChild(box);
+        });
+    } else {
+        const filteredBoxes = [];
         boxes.forEach((box) => {
-            if (continent === "all" || box.getAttribute("data-continent") === continent) {
+            if (box.getAttribute("data-continent") === continent) {
                 box.classList.remove("hidden");
+                filteredBoxes.push(box);
             } else {
                 box.classList.add("hidden");
+                 container.appendChild(box);
             }
         });
-    };
 
-    allButton.addEventListener("click", () => filterBoxes("all"));
-    euButton.addEventListener("click", () => filterBoxes("europe"));
-    naButton.addEventListener("click", () => filterBoxes("north-america"));
-    saButton.addEventListener("click", () => filterBoxes("south-america"));
-    asiaButton.addEventListener("click", () => filterBoxes("asia"));
+      
+
+      
+    }
+};
+
+showAllButton.addEventListener("click", () => filterBoxes("all"));
+euButton.addEventListener("click", () => filterBoxes("europe"));
+naButton.addEventListener("click", () => filterBoxes("north-america"));
+saButton.addEventListener("click", () => filterBoxes("south-america"));
+asiaButton.addEventListener("click", () => filterBoxes("asia"));
